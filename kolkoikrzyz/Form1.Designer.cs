4d252ef5146f69a0beafe9798bb17b2f122ccf8d
Sebastian Skiba <sebastian.skiba@teb.edu.pl>
2022-11-07 15:13:27 +0100

    zmiana

--- kolkoikrzyz/Form1.Designer.cs
+++ kolkoikrzyz/Form1.Designer.cs
@@ -187,7 +187,6 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
--- kolkoikrzyz/Form1.cs
+++ kolkoikrzyz/Form1.cs
@@ -4,6 +4,12 @@ namespace kolkoikrzyz
     public partial class Form1 : Form
     {
         int ktoObecnieGra = 0;
+        int ileNaPlanszy = 0;
+        int[,] plansz = new int[3, 3] {
+            {0, 0, 0} ,   
+            {0, 0, 0} ,   
+            {0, 0, 0}  
+        };
         public Form1()
         {
             InitializeComponent();
@@ -13,11 +19,13 @@ namespace kolkoikrzyz
         private void click(object sender, EventArgs e)
         {
             Button przycisk = (Button)sender;
+            
             if (ktoObecnieGra == 0)
             {
                 przycisk.Image = imageList1.Images[ktoObecnieGra];
                 kto1.Text = "Kó³ko";
                 przycisk.Enabled = false;
+                ileNaPlanszy++;
                 ktoObecnieGra++;
             }
             else if(ktoObecnieGra == 1)
@@ -25,13 +33,15 @@ namespace kolkoikrzyz
                 przycisk.Image = imageList1.Images[ktoObecnieGra];
                 kto1.Text = "Krzy¿yk";
                 przycisk.Enabled = false;
+                ileNaPlanszy++;
                 ktoObecnieGra--;
+            }
+            if(ileNaPlanszy == 9)
+            {
+                // koniec (wyskakuj¹ce okno)
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
